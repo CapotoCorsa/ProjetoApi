dd12a31377accd820ee4c716f1f6c5f255cc296c
Douglas <dodomathias29@gmail.com>
2026-05-22 21:30:19 -0300

    teste

---
+++ projetoapi/src/test/java/br/com/serratec/projetoapi/ProjetoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serratec.projetoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
