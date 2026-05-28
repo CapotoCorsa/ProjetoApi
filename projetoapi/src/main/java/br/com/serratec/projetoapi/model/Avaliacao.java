7f44e04d42d84823dcde3317fdd492d904f8bed9
filipebrollo <filipebrollo@protonmail.com>
2026-05-28 19:26:25 -0300

    update: atualizando a classe Avaliacao com validaç oes

--- projetoapi/src/main/java/br/com/serratec/projetoapi/model/Avaliacao.java
+++ projetoapi/src/main/java/br/com/serratec/projetoapi/model/Avaliacao.java
@@ -3,6 +3,12 @@ package br.com.serratec.projetoapi.model;
 import java.time.LocalDate;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Avaliacao {
@@ -11,10 +17,17 @@ public class Avaliacao {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "A nota é obrigatória")
+    @Min(value = 1, message = "A nota mínima é 1")
+    @Max(value = 5, message = "A nota máxima é 5")
     private Integer nota;
 
+    @NotBlank(message = "O comentário não pode estar vazio")
+    @Size(min = 3, max = 255, message = "O comentário deve ter entre 3 e 255 caracteres")
     private String comentario;
 
+    @NotNull(message = "A data é obrigatória")
+    @PastOrPresent(message = "A data não pode ser no futuro")
     private LocalDate data;
 
     @ManyToOne
