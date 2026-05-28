1f165eb3adc2b1e4694d8d26cc812949dc51a110
NikolasGuerra <nikolasramos7@gmail.com>
2026-05-28 15:50:47 -0300

    adicionado função de notificar o cliente por email toda vez que o status da ordem foi alterado

---
+++ projetoapi/src/main/java/br/com/serratec/projetoapi/service/NotificacaoService.java
@@ -0,0 +1,19 @@
+package br.com.serratec.projetoapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificacaoService {
+    private final EmailService service;
+
+    public NotificacaoService(EmailService service) {
+        this.service = service;
+    }
+    
+    public void notificar(String email, String status) {
+        String mensagem= "O status da ordem de serviço foi atualizado. Favor entrar em contato para mais detalhes.";
+
+        service.enviarEmail(email, status, mensagem);
+    }
+
+}
