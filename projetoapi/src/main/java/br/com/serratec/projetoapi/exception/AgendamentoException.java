d3a9492930bab5b56e4a39b0fcd23eefd6878ff1
nisgri <nisgri@hotmail.com>
2026-05-28 16:50:10 -0300

    adicionado função individual de controle de agendamento

---
+++ projetoapi/src/main/java/br/com/serratec/projetoapi/exception/AgendamentoException.java
@@ -0,0 +1,7 @@
+package br.com.serratec.projetoapi.exception;
+
+public class AgendamentoException extends RuntimeException{
+    public AgendamentoException(String message) {
+        super(message);
+    }
+}
