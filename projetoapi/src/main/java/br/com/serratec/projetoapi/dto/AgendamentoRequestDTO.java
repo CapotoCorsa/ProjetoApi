e06f678340227d41db505cf07aeaf56bed35310d
nisgri <nisgri@hotmail.com>
2026-05-29 11:41:15 -0300

    AgendamentoDTO não pede mais o status (agora faz automático)

--- projetoapi/src/main/java/br/com/serratec/projetoapi/dto/AgendamentoRequestDTO.java
+++ projetoapi/src/main/java/br/com/serratec/projetoapi/dto/AgendamentoRequestDTO.java
@@ -3,12 +3,9 @@ package br.com.serratec.projetoapi.dto;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import br.com.serratec.projetoapi.enums.StatusAgendamento;
-
 public record AgendamentoRequestDTO(
     LocalDate data,
     LocalTime hora,
-    StatusAgendamento status,
     Long veiculoId,
     Long servicoId
 ) {}
